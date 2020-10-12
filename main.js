513ec6f7ea2b51682c2858fd65162ec57ede5ddd
Gabriel Ceni <gceni10@gmail.com>
2020-10-12 11:47:50 -0300

    Part01_03.

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
+/*
+var d=new Date();
+alert(d.getMonth()+1);
+*/
+/*var count;
+for(count=0;count<5;count++){
+    console.log(count);
+}
+*/
+/*var count=0;
+while(count<=5){
+    console.log(count);
+    count++;
+}
+*/
+/*
+var idade=prompt("Idade?")
+if(idade >= 18){
+    alert("Maior de idade");
+}else{
+    alert("Menor de idade");
+}
+*/
+/*
 var fruta=[{nome:"maça",cor:"vermelha"}, {nome:"uva",cor:"roxa"}];
 console.log(fruta);
 alert(fruta[1].nome)
+*/
 /*
 var fruta={nome:"maça",cor:"vermelha"}
 console.log(fruta.cor);
