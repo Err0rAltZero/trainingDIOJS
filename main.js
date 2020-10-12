fc261bc429d0c73de53c647a6b9b6115d8ff692e
Gabriel Ceni <gceni10@gmail.com>
2020-10-12 11:23:59 -0300

    Part01_01.

---
+++ main.js
@@ -0,0 +1,9 @@
+var nome="Gabriel Ceni";
+var val1=20;
+var val2=22;
+var frase="Circle is Red"
+alert("Bem vindo "+nome);
+console.log(nome);
+console.log(val1+val2);
+console.log(frase.toLowerCase());
+//alert(frase.replace("Circle","Square"));
