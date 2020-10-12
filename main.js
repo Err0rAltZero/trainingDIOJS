efd8c315f2afed749d0f4b30b1d9faa91fee2027
Gabriel Ceni <gceni10@gmail.com>
2020-10-12 11:37:57 -0300

    Part01_02

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
-var nome="Gabriel Ceni";
-var val1=20;
-var val2=22;
-var frase="Circle is Red"
-alert("Bem vindo "+nome);
-console.log(nome);
-console.log(val1+val2);
-console.log(frase.toLowerCase());
-//alert(frase.replace("Circle","Square"));
+var fruta=[{nome:"maça",cor:"vermelha"}, {nome:"uva",cor:"roxa"}];
+console.log(fruta);
+alert(fruta[1].nome)
+/*
+var fruta={nome:"maça",cor:"vermelha"}
+console.log(fruta.cor);
+*/
+//var lista=["appel","pineapple","orange"];
+//lista.push("grape");
+//lista.pop("grape");
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" | "));
+//var nome="dsadsa"
+//var n1=5;
+//var n2=3;
+//var frase="sadsawwawqwq"
+//alert(nome+" tem "+n1+" anos");
+//alert(n1+n2);
+//consoloe.log(nome);
+//console.log(n1*n2);
+//console.log(frase.toLowerCase());
